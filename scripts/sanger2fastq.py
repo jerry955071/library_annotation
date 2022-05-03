45c7832b7e7387ef0a65faf7aa7517984681e1af
jerry955071 <jerry955071@gmail.com>
2022-05-03 15:48:45 +0800

    'in_dir/*.ab1' > 'out_dir/*.fq'; use`pattern_rev`

--- scripts/sanger2fastq.py
+++ scripts/sanger2fastq.py
@@ -1,68 +1,81 @@
+"""
+1. Convert AB1 to FASTQ.
+2. Replace file name typo ('-' -> '_')
+3. Reverse complement reverse sequences (recognized by *PATTERN*)
+"""
+
 from Bio import SeqIO
 import argparse
 import os
+import sys
+import re
 
 # create ArgumentParser
 parser = argparse.ArgumentParser(description='Reminder:')
-parser.add_argument("-i", 
-    help="folder containing sanger sequencing results", type=str)
-parser.add_argument("--out1", help="output read1 file name", type=str)
-parser.add_argument("--out2", help="output read2 file name", type=str)
-parser.add_argument("--suff1", help="input read1 file suffix", type=str)
-parser.add_argument("--suff2", help="input read2 file suffix", type=str)
-
+parser.add_argument("--in_dir", help="input .ab1 directory", type=str)
+parser.add_argument("--out_dir", help="output .fq directory", type=str)
+parser.add_argument(
+    "--pattern_rev",
+    help="the pattern used to recognize reverse sequencese",
+    type=str,
+    default=".*(ADR).*"
+    )
 
 # get parsed command line arguments
 args = parser.parse_args()
-in_dir = args.i
-out1 = args.out1
-out2 = args.out2
-suff1 = args.suff1
-suff2 = args.suff2
-# in_dir = "raw_sanger"
-# out1 = "raw_fastq/read1.fq"
-# out2 = "raw_fastq/read2.fq"
-# suff1 = "ADF.ab1"
-# suff2 = "ADR.ab1"
+in_dir = args.in_dir
+out_dir = args.out_dir
+pattern_rev = args.pattern_rev
 
+# create reverse file name parser
+prog = re.compile(pattern_rev)
 
-# create files
-handle1 = open(out1, "w").close()
-handle2 = open(out2, "w").close()
-
-
-# check sequencing results files
+# list all sequencing results files
 flist = os.listdir(in_dir)
 flist.sort()
 
+# create output directory (exist_ok)
+os.makedirs(out_dir, exist_ok=True)
 
-# write .ab1 to .fq
-with open(out1, "a") as handle1, open(out2, "a") as handle2:
-    flist = os.listdir(in_dir)
-    flist.sort()
-    for fname in flist:
-        if suff1 in fname:
-            record = SeqIO.read(f"{in_dir}/{fname}", "abi")
-            # some record.id were mis-named use record.name instead
-            record.id = record.name
-            # remove suffix
-            record.id = record.id.split("_ADF")[0]
-            # replace "-" with "_" in mis-named file names
-            if "-" in record.id:
-                record.id = record.id.replace("-", "_")
-            # output to file
-            foo = SeqIO.write(record, handle1, "fastq") \
-                # `foo` were used to catch the output `1` from `SeqIO.write()`
+# 
+n_abi = 0
+n_fq = 0
+for fname in flist:
+    # skip not '.ab1' files
+    if not ".ab1" in fname:
+        continue
+    
+    n_abi += 1
+    # get file base name
+    bname = fname.split(".")[0]
 
-        if suff2 in fname:
-            record = SeqIO.read(f"{in_dir}/{fname}", "abi")
-            # some record.id were mis-named use record.name instead
+    # 'abi' to 'fastq'
+    with open(f"{out_dir}/{bname}.fq", "w") as handle_out:
+        # get abi record
+        record = SeqIO.read(f"{in_dir}/{fname}", "abi")
+        
+        # replace typo
+        if "-" in record.name:
+            record.id = record.name.replace("-", "_")
+        else:
             record.id = record.name
-            # remove suffix
-            record.id = record.id.split("_ADR")[0]
-            # replace "-" with "_" in mis-named file names
-            if "-" in record.id:
-                record.id = record.id.replace("-", "_")
-            # output to file
-            foo = SeqIO.write(record, handle2, "fastq") \
-                # `foo` were used to catch the output `1` from `SeqIO.write()`
+
+        # reverse reverse sequences
+        if prog.match(record.id):
+            rev = record.reverse_complement()
+            rev.id = record.id
+            rev.name = record.name
+            rev.description = ""
+            record = rev
+        
+        # output to 'fastq'
+        foo = SeqIO.write(record, handle_out, "fastq")
+        n_fq += 1
+
+print(f"""\
+INPUT DIRECTORY:\t'{in_dir}'
+OUTPUT DIRECTORY:\t'{out_dir}'
+INPUT AB1 FILES:\t{n_abi}
+OUTPUT FASTQ FILES:\t{n_fq}""", file=sys.stdout
+    )
+        
